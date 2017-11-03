9fe29bc15471e6bba1b8be34ddd0397ffc229740
JoaoHergert <joaohergert@gmail.com>
2017-11-03 17:14:58 -0200

    Initial Commit

---
+++ CestaDoCampo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CestaDoCampo
+//
+//  Created by João Agostinho Hergert on 03/11/17.
+//  Copyright © 2017 João Hergert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
