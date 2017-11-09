b0dcb1d051cd87c0e1c1236433f0cbc1f06ece6e
Edgar Silva <edgar@10-40-40-46.0101.ecps.br.iosda.org>
2017-11-09 17:16:48 -0200

    Adding the pickup point model

---
+++ Model/PickupDay.swift
@@ -0,0 +1,9 @@
+//
+//  PickupDay.swift
+//  CestaDoCampo
+//
+//  Created by Edgar Silva on 09/11/17.
+//  Copyright © 2017 João Hergert. All rights reserved.
+//
+
+import Foundation
---
+++ Model/PickupPoint.swift
@@ -0,0 +1,38 @@
+//
+//  PickupPoint.swift
+//  CestaDoCampo
+//
+//  Created by Edgar Silva on 09/11/17.
+//  Copyright © 2017 João Hergert. All rights reserved.
+//
+
+import UIKit
+
+/// The pickup point model
+class PickupPoint {
+
+    /// Id of the pickup point
+    public var id: Int?
+
+    /// Name of the pickup point
+    public var name: String?
+
+    /// Address Street name
+    public var address: String?
+
+    /// City of pickup point
+    public var city: String?
+
+    /// State Address of producer
+    public var state: String?
+
+    /// Country of producer
+    public var country: String?
+
+    /// Latitude of the pickup point
+    public var latitude: Double?
+
+    /// Longitude of the pickup point
+    public var longitude: Double?
+
+}
