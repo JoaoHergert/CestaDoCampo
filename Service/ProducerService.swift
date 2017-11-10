4e5ba109a63494bf77f1543175ed338db28e74a8
Edgar Silva <edgar@10-40-49-99.0200.ecps.br.iosda.org>
2017-11-10 11:49:16 -0200

    Adding producer service methods signature

---
+++ Service/ProducerService.swift
@@ -0,0 +1,19 @@
+//
+//  ProducerService.swift
+//  CestaDoCampo
+//
+//  Created by Edgar Silva on 09/11/17.
+//  Copyright © 2017 João Hergert. All rights reserved.
+//
+
+import UIKit
+
+class ProducerService {
+    func search(term: String, onlyDelivery: Bool, latitude: Double, longitude: Double) -> [Producer] {
+        let result = [Producer]()
+
+        return result
+    }
+}
+
+
